1c23373ff3adf5beb889914b3113e7201ff24452
Joe McCann <joseph.isaac@gmail.com>
2013-04-10 22:00:32 -0500

    version bump

--- public/js/boot.prod.js
+++ public/js/boot.prod.js
@@ -2,7 +2,7 @@
 var $lib = ('__proto__' in {} ? 'zepto.min' : 'jquery.min')
 require(["log",  $lib, "fastclick" ], function(l, zepto){
   log($lib + ', Fastclick and Log loaded...')
-  require(["kloutskout-0.0.1-2.min"], function(kloutskout){
+  require(["kloutskout-0.0.1-3.min"], function(kloutskout){
 	  new FastClick(document.body)
 		log('FastClick enabled on document.')
 		log('All JS files loaded...')
